d8ef8f6634caf76f639e9b6a0c9e22f61436246a
Məhəmməd <mahammad.c@code.edu.az>
2017-07-25 20:59:54 +0500

    greenside

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,23 +2,26 @@ $(document).ready(function(){
 	$(window).scroll(function(event) {
 		var animate= $(window).scrollTop();
 		console.log(animate);
-
 		if (animate>220) {
 			$(".navbar_change").css({
-        background:'black',
+        background:'#1b6d3f',
         width:'100%',
-        height:'57px',
+        height:'80px',
         position:'fixed',
         top:'0px',
-
 			});
+      $("header").css({
+        paddingTop:'15px',
+      });
 		}
 		if (animate==0) {
 			$(".navbar_change").css({
 				background:'transparent',
 			});
 		}
-
-
 	});
+});
+
+$(function(){
+  $.scrollIt();
 });
